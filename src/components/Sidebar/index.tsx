95f5227554aff99c9eec29b615652e685a227669
KumarBalajiNagaraj <balaji.volvocars@gmail.com>
2024-06-06 13:39:23 +0200

    logo chanhes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -316,7 +316,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             <Image
               width={176}
               height={32}
-              src={"/images/logo/logo-dark.svg"}
+              src={"/images/logo/logo.png"}
               alt="Logo"
               priority
               className="dark:hidden"
